ad347a8ec472e24f9db62fd71da834b5cba03ed2
liuyuan <107234685+liuyuayua@users.noreply.github.com>
2023-06-06 16:04:41 +0100

    Create themeConfig.ts

---
+++ docs/.vuepress/themeConfig.ts
@@ -0,0 +1,122 @@
+import { defineThemeConfig } from "vuepress-theme-hope";
+import { navbarConfig } from "./navbar";
+import { sidebarConfig } from "./sidebar";
+
+export default defineThemeConfig({
+  logo: "/logo.png",
+  hostname: "https://javaguide.cn/",
+  author: {
+    name: "Guide",
+    url: "https://javaguide.cn/",
+  },
+  repo: "https://github.com/Snailclimb/JavaGuide",
+  docsDir: "docs",
+  iconPrefix: "iconfont icon-",
+  pure: true,
+  navbar: navbarConfig,
+  sidebar: sidebarConfig,
+  pageInfo: ["Author", "Category", "Tag", "Date", "Original", "Word"],
+  blog: {
+    intro: "/about-the-author/",
+    sidebarDisplay: "mobile",
+    medias: {
+      Zhihu: "https://www.zhihu.com/people/javaguide",
+      Github: "https://github.com/Snailclimb",
+      Gitee: "https://gitee.com/SnailClimb",
+    },
+  },
+  footer:
+    '<a href="https://beian.miit.gov.cn/" target="_blank">鄂ICP备2020015769号-1</a>',
+  displayFooter: true,
+  plugins: {
+    blog: {
+      autoExcerpt: true,
+    },
+    mdEnhance: {
+      tasklist: true,
+    },
+    feed: {
+      json: true,
+    },
+    // comment: {
+    //   type: "giscus",
+    //   repo: "Snailclimb/JavaGuide",
+    //   repoId: "MDEwOlJlcG9zaXRvcnkxMzI0NjQzOTU=",
+    //   category: "Announcements",
+    //   categoryId: "DIC_kwDOB-U_C84COYQF",
+    // },
+    search: {
+      // https://v2.vuepress.vuejs.org/zh/reference/plugin/search.html
+      // 排除首页
+      isSearchable: (page) => page.path !== "/",
+      maxSuggestions: 10,
+      hotKeys: ["s", "/"],
+      // 用于在页面的搜索索引中添加额外字段
+      getExtraFields: () => [],
+      locales: {
+        "/": {
+          placeholder: "搜索",
+        },
+      },
+    },
+    // pwa: {
+    //   favicon: "/favicon.ico",
+    //   cachePic: true,
+    //   apple: {
+    //     icon: "/assets/icon/apple-icon-152.png",
+    //     statusBarColor: "black",
+    //   },
+    //   msTile: {
+    //     image: "/assets/icon/ms-icon-144.png",
+    //     color: "#ffffff",
+    //   },
+    //   manifest: {
+    //     icons: [
+    //       {
+    //         src: "/assets/icon/chrome-mask-512.png",
+    //         sizes: "512x512",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-mask-192.png",
+    //         sizes: "192x192",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //     shortcuts: [
+    //       {
+    //         name: "Guide",
+    //         short_name: "Guide",
+    //         url: "/guide/",
+    //         icons: [
+    //           {
+    //             src: "/assets/icon/guide-maskable.png",
+    //             sizes: "192x192",
+    //             purpose: "maskable",
+    //             type: "image/png",
+    //           },
+    //           {
+    //             src: "/assets/icon/guide-monochrome.png",
+    //             sizes: "192x192",
+    //             purpose: "monochrome",
+    //             type: "image/png",
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // },
+  },
+});
