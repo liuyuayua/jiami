7b7dfb5e3239be5b79ad67216a1ea0328e6dfcb1
Duranzecaria <121248281+Duranzecaria@users.noreply.github.com>
2025-11-21 17:15:50 +0800

    Update run.sh

--- test/skipper/run.sh
+++ test/skipper/run.sh
@@ -9,3 +9,13 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 "$REPO_ROOT"/test/workloads/init.sh
 "$DIR"/test-canary.sh
+if [ $LOGROUTER_COUNT -gt 0 ]; then
+	start_servers $LOGROUTER_COUNT "$STORAGE_TASKSET" router DC3
+	# Same number remote/satellite logs and ss as primary
+	start_servers $LOGS_COUNT "$LOGS_TASKSET" log DC2
+	start_servers $LOGS_COUNT "$LOGS_TASKSET" log DC3
+	start_servers $STORAGE_COUNT "$STORAGE_TASKSET" storage DC3
+	create_fileconfig
+	$CLI "fileconfigure /tmp/fdbfileconfig.json"
+	echo "Wait for data to be fully replicated (Healthy), then issue: $CLI configure usable_regions=2"
+fi
